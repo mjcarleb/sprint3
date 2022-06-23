141dce76c5c7049993ace09f54191abf2da4c2bb
Mark Carlebach <markcarlebach@marks-mbp.myfiosgateway.com>
2022-06-23 08:58:57 -0400

    not working but as close as I can get after failed demo

--- celery_tasks.py
+++ celery_tasks.py
@@ -1,5 +1,6 @@
 from celery import Celery
 import sys
+import asyncio
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
@@ -12,5 +13,40 @@ def exit(x, y):
     sys.exit(0)
 
 @app.task
-def clear_c8_task(x, y):
-    sys.exit(0)
+def clear_c8_task(webhook_id):
+
+    from pyzeebe import ZeebeWorker, create_insecure_channel
+
+    channel = create_insecure_channel(
+        hostname='44.199.120.6',
+        port=26500
+    )
+
+    worker = ZeebeWorker(grpc_channel=channel)
+
+    @worker.task(task_type=f"{webhook_id}")
+    def execute_webhook(url, method, webhook_uuid):
+        # Now that Jira has returned the webhook_id, we just clear the task on Zeebe with return
+        return {}
+
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    loop.run_until_complete(worker.work(one_shot=True))
+
+    channel = create_insecure_channel(
+        hostname='44.199.120.6',
+        port=26500
+    )
+
+    #pid = self.pid
+    worker = ZeebeWorker(grpc_channel=channel)
+
+    @worker.task(task_type=f"{webhook_id}")
+    def execute_webhook(url, method, webhook_uuid):
+        # Now that Jira has returned the webhook_id, we just clear the task on Zeebe with return
+        print('clearing awaiting task in C8')
+        return {}
+
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    loop.run_until_complete(worker.work(one_shot=True))
