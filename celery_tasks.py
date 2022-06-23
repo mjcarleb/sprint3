74398908766b82bf305c766c193669b33936c989
Mark Carlebach <markcarlebach@marks-mbp.myfiosgateway.com>
2022-06-23 07:50:11 -0400

    Revert "working prior to email"
    This reverts commit f68a7053f27657b8d647b1550f456d91c4cb3b81.

--- celery_tasks.py
+++ celery_tasks.py
@@ -1,7 +1,5 @@
 from celery import Celery
 import sys
-from pyzeebe import ZeebeWorker, create_insecure_channel
-import asyncio
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
@@ -14,19 +12,5 @@ def exit(x, y):
     sys.exit(0)
 
 @app.task
-def clear_c8_task(webhook_id):
-    channel = create_insecure_channel(
-        hostname='44.199.120.6',
-        port=26500
-    )
-
-    worker = ZeebeWorker(grpc_channel=channel)
-
-    @worker.task(task_type=f"{webhook_id}")
-    def execute_webhook(url, method, webhook_uuid):
-        # Now that Jira has returned the webhook_id, we just clear the task on Zeebe with return
-        return {}
-
-    loop = asyncio.get_event_loop()
-    loop.set_debug(True)
-    loop.run_until_complete(worker.work(one_shot=True))
+def clear_c8_task(x, y):
+    sys.exit(0)
